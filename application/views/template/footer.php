92e6527443f53df8d009498f7add54e006b59a84
AndikOffice <andik.aryantoo@gmail.com>
2018-12-19 09:29:05 +0700

    fix scroll bar
    fix scroll bar

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -35,10 +35,57 @@
         </div>
       </footer>
       <!-- <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 0px;"><div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;"></div></div>
-      <div class="ps-scrollbar-y-rail" style="top: 0px; right: 0px; height: 626px;"><div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 494px;"></div></div>
-      <div class="close-layer visible"></div> -->
+      <div class="ps-scrollbar-y-rail" style="top: 0px; right: 0px; height: 626px;"><div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 317px;"></div></div> -->
     </div>
   </div>
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/core/popper.min.js');?>"></script>
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/core/bootstrap-material-design.min.js');?>"></script>
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/perfect-scrollbar.jquery.min.js');?>"></script>
+  <!-- Plugin for the momentJs  -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/moment.min.js');?>"></script>
+  <!--  Plugin for Sweet Alert -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/sweetalert2.js');?>"></script>
+  <!-- Forms Validations Plugin -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/jquery.validate.min.js');?>"></script>
+  <!-- Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/jquery.bootstrap-wizard.js');?>"></script>
+  <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/bootstrap-selectpicker.js');?>"></script>
+  <!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/bootstrap-datetimepicker.min.js');?>"></script>
+  <!--  DataTables.net Plugin, full documentation here: https://datatables.net/  -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/jquery.dataTables.min.js');?>"></script>
+  <!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/bootstrap-tagsinput.js');?>"></script>
+  
+  
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/jasny-bootstrap.min.js');?>"></script>
+  <!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/jasny-bootstrap.min.js');?>"></script>
+  <!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/fullcalendar.min.js');?>"></script>
+  <!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/jquery-jvectormap.js');?>"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/nouislider.min.js');?>"></script>
+  <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
+  <!-- Library for adding dinamically elements -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/arrive.min.js');?>"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <script async="" defer="" src="https://buttons.github.io/buttons.js"></script>
+  <!-- Chartist JS -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/chartist.min.js');?>"></script>
+  <!--  Notifications Plugin    -->
+  <!-- <script src="<?php echo base_url('assets/material-dashboard/assets/js/plugins/bootstrap-notify.js');?>"></script> -->
+  <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+  <!-- script src="<?php echo base_url('assets/material-dashboard/assets/js/material-dashboard.min.js');?>"></script> -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/js/material-dashboard.min.js');?>"></script>
+  
+  <!-- Material Dashboard DEMO methods, don't include it in your project! -->
+  <script src="<?php echo base_url('assets/material-dashboard/assets/demo/demo.js');?>"></script>
+  <script src="<?php echo base_url('assets/material-dashboard/assets/demo/jquery.sharrre.js');?>"></script>
   <script>
     $(document).ready(function() {
       $().ready(function() {
